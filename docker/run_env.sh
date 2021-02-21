7233da280cce9feefd8601036975332736eff054
shinyeop.lee <leesy7070@gmail.com>
2021-02-21 19:54:05 +0900

    add error handling

--- docker/run_env.sh
+++ docker/run_env.sh
@@ -2,9 +2,28 @@
 PARENT_DIR_PATH=$(dirname $PWD)
 PROJECT_NAME=$(basename $PARENT_DIR_PATH)
 
-docker ps -a | grep -w ${PROJECT_NAME} | awk '{print $1}' | xargs docker stop
+# kill old containers
+echo "stop old ${PROJECT_NAME} env ..."
+OLD_CONTAILERS=$(docker ps -a | grep -w ${PROJECT_NAME})
+if [ $? != 0 ]; then
+    echo "Failed to get old containers"
+elif [ -n "$OLD_CONTAILERS" ]; then
+    echo "$OLD_CONTAILERS" | awk '{print $1}' | xargs docker stop
+    echo "Succeed to stop old containers"
+else
+    echo "No old containers found"
+fi
+echo "----------------------------------"
+
+# run new container
+echo "start ${PROJECT_NAME} env ..."
 docker run -it -d --rm --name ${PROJECT_NAME} \
     -v ${PARENT_DIR_PATH}:/home/project \
     ${PROJECT_NAME}
 
-docker exec -it ${PROJECT_NAME} bash
+if [ $? -eq 0 ]; then
+    docker exec -it ${PROJECT_NAME} bash
+else
+   echo "Failed to run ${PROJECT_NAME} env"
+fi
+echo "----------------------------------"
